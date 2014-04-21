1c7725763eafe2e52df9eaf389c6b586c88782d2
Brett Lesnau <lesnaubr@gmail.com>
2014-04-20 20:51:06 -0400

    break again

--- MobileService/DrinkWithMeService/Global.asax.cs
+++ MobileService/DrinkWithMeService/Global.asax.cs
@@ -7,7 +7,7 @@ namespace drinkwithmeService
     {
         protected void Application_Start()
         {
-            WebApiConfig.Register ();
+            WebApiConfig.Regisgter();
         }
     }
 }
