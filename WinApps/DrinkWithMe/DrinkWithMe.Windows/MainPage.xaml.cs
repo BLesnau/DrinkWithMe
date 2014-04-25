668dbb59caee648955abba189877fd75b36724b5
Brett Lesnau <lesnaubr@gmail.com>
2014-04-24 23:32:15 -0400

    Mobile Service authentication works

--- WinApps/DrinkWithMe/DrinkWithMe.Windows/MainPage.xaml.cs
+++ WinApps/DrinkWithMe/DrinkWithMe.Windows/MainPage.xaml.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace DrinkWithMe
 {
@@ -25,6 +16,28 @@ namespace DrinkWithMe
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Click( object sender, Windows.UI.Xaml.RoutedEventArgs e )
+        {
+           try
+           {
+              var app = Application.Current as App;
+              if ( app != null )
+              {
+                 if ( app.MobileService != null )
+                 {
+                    var login = await app.MobileService.LoginAsync( MobileServiceAuthenticationProvider.Google );
+                    var m = new MessageDialog( String.Format( "{0} {1}", login.UserId, login.MobileServiceAuthenticationToken ), "Title" );
+                    await m.ShowAsync();
+                 }
+              }
+           }
+           catch ( Exception ex )
+           {
+              var m = new MessageDialog( ex.Message, "Exception" );
+              m.ShowAsync();
+           }
         }
     }
 }
--- WinApps/DrinkWithMe/DrinkWithMe.WindowsPhone/MainPage.xaml.cs
+++ WinApps/DrinkWithMe/DrinkWithMe.WindowsPhone/MainPage.xaml.cs
@@ -1,48 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace DrinkWithMe
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
+   /// <summary>
+   /// An empty page that can be used on its own or navigated to within a Frame.
+   /// </summary>
+   public sealed partial class MainPage : Page
+   {
+      public MainPage()
+      {
+         this.InitializeComponent();
 
-            this.NavigationCacheMode = NavigationCacheMode.Required;
-        }
+         this.NavigationCacheMode = NavigationCacheMode.Required;
+      }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            // TODO: Prepare page for display here.
+      /// <summary>
+      /// Invoked when this page is about to be displayed in a Frame.
+      /// </summary>
+      /// <param name="e">Event data that describes how this page was reached.
+      /// This parameter is typically used to configure the page.</param>
+      protected override void OnNavigatedTo( NavigationEventArgs e )
+      {
+         // TODO: Prepare page for display here.
 
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
-        }
-    }
+         // TODO: If your application contains multiple pages, ensure that you are
+         // handling the hardware Back button by registering for the
+         // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+         // If you are using the NavigationHelper provided by some templates,
+         // this event is handled for you.
+      }
+
+      private async void Button_Click( object sender, RoutedEventArgs e )
+      {
+         try
+         {
+            var app = Application.Current as App;
+            if ( app != null )
+            {
+               if ( app.MobileService != null )
+               {
+                  var login = await app.MobileService.LoginAsync( MobileServiceAuthenticationProvider.Google );
+                  var m = new MessageDialog( String.Format( "{0} {1}", login.UserId, login.MobileServiceAuthenticationToken ), "Title" );
+                  await m.ShowAsync();
+               }
+            }
+         }
+         catch ( Exception ex)
+         {
+            var m = new MessageDialog(ex.Message,"Exception");
+            m.ShowAsync();
+         }
+      }
+   }
 }
